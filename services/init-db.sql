e34d5c6a0f29990c20bb085a4f2a85f1fa12a2ab
Sagar S H <158994310+Sagarrrsh@users.noreply.github.com>
2026-01-24 19:13:42 +0530

    Update init-db.sql

--- services/init-db.sql
+++ services/init-db.sql
@@ -1,7 +1,3 @@
-CREATE DATABASE IF NOT EXISTS scrapzee_auth;
-CREATE DATABASE IF NOT EXISTS scrapzee_pricing;
-CREATE DATABASE IF NOT EXISTS scrapzee_users;
-
-ALTER DATABASE scrapzee_auth CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER DATABASE scrapzee_pricing CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER DATABASE scrapzee_users CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS scrapzee
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
